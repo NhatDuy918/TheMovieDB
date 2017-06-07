355ea5c2233a16e3af45f574917e0c5f6c2f1b23
xcode <nhatduy918@gmail.com>
2017-06-07 14:07:54 +0700

    Initial Commit

---
+++ MovieDB/MovieCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDB.swift
+//  MovieDB
+//
+//  Created by Cntt02 on 5/3/17.
+//  Copyright © 2017 Cntt02. All rights reserved.
+//
+
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var poster: UIImageView!
+    @IBOutlet var title: UILabel!
+    @IBOutlet var releaseDate: UILabel!
+}
---
+++ MovieDB/SearchCollectionReusableView.swift
@@ -0,0 +1,14 @@
+//
+//  MovieDB.swift
+//  MovieDB
+//
+//  Created by Cntt02 on 5/3/17.
+//  Copyright © 2017 Cntt02. All rights reserved.
+//
+
+import UIKit
+
+class SearchCollectionReusableView: UICollectionReusableView {
+        
+    @IBOutlet var searchBar: UISearchBar!
+}
